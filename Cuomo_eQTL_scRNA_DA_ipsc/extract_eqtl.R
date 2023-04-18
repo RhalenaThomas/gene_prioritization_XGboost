47b07bc1ec5182bbd00d5325ba4b8fbc8d7087a7
Eric Yu <32378135+eyu8@users.noreply.github.com>
2023-04-18 13:02:08 -0400

    Update extract_eqtl.R

--- Cuomo_eQTL_scRNA_DA_ipsc/extract_eqtl.R
+++ Cuomo_eQTL_scRNA_DA_ipsc/extract_eqtl.R
@@ -7,6 +7,7 @@ locus <- locus[,c("symbol","locus")]
 
 data <- fread("D30.DA.qtl_results_all.sorted_gwas_snp_after_fdr.txt")
 
+#Query for HGNC symbols
 ensembl <- useEnsembl(biomart = "genes", dataset = "hsapiens_gene_ensembl", version = "GRCh37")
 gene <- getBM(attributes = c('ensembl_gene_id','hgnc_symbol'), filters = c('ensembl_gene_id', 'biotype'), values = list(data$feature_id, biotype="protein_coding"), mart = ensembl)
 
@@ -14,12 +15,14 @@ colnames(gene) <- c("feature_id", "symbol")
 
 final <- merge(data, gene)
 
+#Calculate mean effect
 result <- final %>% group_by(symbol) %>% summarize(Cuomo_MEAN_EFFECT = mean(beta), Cuomo_MAX_EFFECT = max(beta))
 
 final22 <- merge(result, locus)
 
 write.table(final22, "D30.DA.qtl_results_all.sorted_gwas_snp_summarized.txt", quote = F, row.names = F, sep = "\t")
 
+#Calculate neighborhood scores
 final2_subset.list <- lapply(sort(unique(final22$locus)), function(i){
                 final2 <- subset(final22, locus == i)
                 colnames(final2) <- paste0(colnames(final2),"_NBH")
